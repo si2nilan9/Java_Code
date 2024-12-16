71e44cf1f1a23f8b92a6c9000d41fcde8b3311f1
si2nilan9 <165579955+si2nilan9@users.noreply.github.com>
2024-12-16 09:43:24 +0530

    Update GUVI_hello.java

--- GUVI_hello.java
+++ GUVI_hello.java
@@ -1,12 +1,31 @@
 package JAVA_CODE;
 
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class GUVI_hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println();
+		// Initialize the variables
+		int num = 62;
+		float pi = 3.14f;
+		double db = 18222.555d;
+		short $Num = 123;
+		long _$LNum = 2908000000000L;
+		boolean bool = true;
+		byte Byte = -128; // Range -128 to 127
+		char letter = 'L';
+		System.out.println("NUMBER : " +num);
+		System.out.println("PI VALUE : "+pi);
+		System.out.println("BOOLEAN : "+bool);
+		System.out.println("DOUBLE VALUE : "+db);
+		System.out.println("LETTER: " +letter);
+		System.out.println("SHORT NUM : " +$Num);
+		System.out.println("LONG NUMBER : " +_$LNum);
+		System.out.println("BYTE VALUE: " +Byte);
 	}
 
 }
